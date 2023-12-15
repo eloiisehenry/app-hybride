a89b081af0cbdc89de945977b6015835666ebd88
Éloïse <101328483+eloiisehenry@users.noreply.github.com>
2023-12-15 16:47:39 +0100

    v

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        style={{ width: 200, height: 200, marginBottom: 20 }}
+        source={require('./assets/icon-weather.png')}
+      />
       <StatusBar style="auto" />
+
+      {/* Input pour le prénom de l'utilisateur */}
+      <View style={styles.textInput}>
+        <TextInput
+          style={{ borderWidth: 1, padding: 10 }}
+          placeholder="Prenom"
+          onChangeText={(val) => setName(val)}
+        />
+      </View>
+
+      {/* Espacement */}
+      <View style={{ marginVertical: 10 }} />
+
+      {/* Bouton pour valider */}
+      <Button
+        style={{ borderWidth: 1, padding: 1, marginBottom: 2, backgroundColor:'rgba(31, 31, 31, 1)' }}
+        title="Valider"
+        onPress={() => {
+          alert(`Bonjour ${name} !`);
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +40,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor:'rgba(31, 31, 31, 1)',
+  },
+  textInput: {
+    width: '100%',
+    marginBottom: 20,
   },
 });
+
